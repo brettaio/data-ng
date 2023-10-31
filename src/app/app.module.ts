7ec83de48ccc7a4c4ef744f0c733d7d04ec70c40
Brett Connell <dev@bretta.io>
2023-10-31 12:41:41 -0400

    started recreating scss into minimalist form to completely bottom required assets and jack load speed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PreloaderComponent } from './preloader/preloader.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PreloaderComponent
+    PreloaderComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
